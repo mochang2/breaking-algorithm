e8f0093d43086c658106ba963661bf0454042bbd
mochang2 <63287638+mochang2@users.noreply.github.com>
2020-09-27 09:04:57 +0900

    Add files via upload
    upload 0925 Q4

---
+++ 0925 Q4.py
@@ -0,0 +1,46 @@
+"""
+섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때,
+모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록
+solution 함수를 작성해주세요.
+
+스코빌 지수가 1인 음식과 2인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+새로운 음식의 스코빌 지수 = 1 + (2 * 2) = 5
+가진 음식의 스코빌 지수 = [5, 3, 9, 10, 12]
+
+스코빌 지수가 3인 음식과 5인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
+새로운 음식의 스코빌 지수 = 3 + (5 * 2) = 13
+가진 음식의 스코빌 지수 = [13, 9, 10, 12]
+
+모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
+"""
+
+
+import heapq as h
+
+def solution(scoville, K):
+    answer = 0
+    
+    h.heapify(scoville)
+
+    while True:
+        small_1 = h.heappop(scoville)
+
+        if small_1 >= K:
+            break
+        else:
+            if len(scoville) == 0:
+                return -1
+            small_2 = h.heappop(scoville)
+            new_one = small_1 + 2 * small_2
+            h.heappush(scoville, new_one)
+            answer+=1
+        
+    return answer
+
+#main.py
+li = [1,2,3,9,10,12]
+result = solution(li, 14)
+
+print(result)
